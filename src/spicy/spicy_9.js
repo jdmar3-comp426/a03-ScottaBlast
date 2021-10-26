bfbed0311c985a217c2107e4d97fe3ebf3114aad
ScottaBlast <you@example.com>
2021-10-26 17:55:12 -0400

    mild medium done autograder borken

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -15,7 +15,11 @@
  *                from calling the function
  */
 export const repeat = (fn, n, ...params) => {
-
+    //call fn n times with params array
+    for(let i = 0; i < n; i++)
+    {
+        
+    }
 };
 
 
